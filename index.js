2a9554e5079ec9b3b57988d5f6cd1dffa194cdb3
skolom <nanaberimaoseiyaw@gmail.com>
2023-12-18 12:59:22 +0000

    html,css and js for BMI-calculator

---
+++ index.js
@@ -0,0 +1,26 @@
+const btnEl = document.getElementById("btn");
+const bmiInputEl = document.getElementById("bmi-result");
+
+
+function calculateBMI() {
+    const heightValue = document.getElementById("height").value / 100;
+    const weightValue = document.getElementById("weight").value;
+
+    const weightConditionEl = document.getElementById("weight-condition");
+
+    const bmiValue = weightValue / (heightValue * heightValue);
+
+    bmiInputEl.value = (bmiValue).toFixed(2);
+
+    if (bmiValue < 18.5) {
+        weightConditionEl.innerHTML = "Under Weight"
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+        weightConditionEl.innerHTML = "Normal weight";
+    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+        weightConditionEl.innerHTML = "Overweight";  
+    } else if (bmiValue >= 30) {
+        weightConditionEl.innerHTML = "Obesity";
+    }
+}
+
+btnEl.addEventListener("click", calculateBMI);
