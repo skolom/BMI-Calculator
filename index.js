eb531dbf03abd75a0e83b3a3602796815504812e
skolom <nanaberimaoseiyaw@gmail.com>
2023-12-18 13:20:19 +0000

    Corrected id for Weight and declared weight condition as const

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const btnEl = document.getElementById("btn");
 const bmiInputEl = document.getElementById("bmi-result");
+const weightConditionEl = document.getElementById("weight-condition");
 
 
 function calculateBMI() {
     const heightValue = document.getElementById("height").value / 100;
     const weightValue = document.getElementById("weight").value;
-
-    const weightConditionEl = document.getElementById("weight-condition");
 
     const bmiValue = weightValue / (heightValue * heightValue);
 
